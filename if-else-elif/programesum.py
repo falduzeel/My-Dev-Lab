c544ea163092966a22669050a18b61535b731260
Zeel faldu <falduzeel54@gmail.com>
2026-04-28 16:32:47 +0530

    Add files via upload

---
+++ if-else-elif/programesum.py
@@ -0,0 +1,10 @@
+num =oNum= int(input("Enter any number :"))
+sum =0
+
+while num !=0:
+    sum += num%10
+    num //=10
+    
+    print(f" sum of each digits of {oNum} is {sum}")
+        
+          
